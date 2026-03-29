52b7a996d0c8b54a8dd7aeec93f4842f6d7b589e
DylanFerger <fergdyl@gmail.com>
2026-03-29 12:42:04 -0400

    testing

--- server.js
+++ server.js
@@ -6,181 +6,184 @@ const path = require('path')
 const { MongoClient } = require('mongodb')
 
 let dbConnection
-let uri = 'mongodb+srv://liftingApp:password123!@lifting-app-db.o3bapio.mongodb.net/?appName=lifting-app-db'
+let db = 'mongodb+srv://liftingApp:password123!@lifting-app-db.o3bapio.mongodb.net/?appName=lifting-app-db'
 
 app.use(cors())
-const lifts = {
-    'slight-incline-db-press': {
-        'Max': 0
-    },
-    'seated-cable-chest-flyes': {
-        'Max': 0
-    },
-    'dip-machine': {
-        'Max': 0
-    },
-    'high-incline-db-press': {
-        'Max': 0
-    },
-    'lateral-raises-(db)': {
-        'Max': 0
-    },
-    'around-the-world-superman-hold': {
-        'Max': 0
-    },
-    'single-arm-pulldown-(lats)': {
-        'Max': 0
-    },
-    'single-arm-cable-row': {
-        'Max': 0
-    },
-    'wide-grip-pulldowns': {
-        'Max': 0
-    },
-    'chest-supported-upper-back-rows': {
-        'Max': 0
-    },
-    'dumbbell-shrug': {
-        'Max': 0
-    },
-    'rear-delt-flye-machine': {
-        'Max': 0
-    },
-    'db-standing-curls': {
-        'Max': 0
-    },
-    'db-hammer-curls': {
-        'Max': 0
-    },
-    'reverse-grip-ez-bar-curls': {
-        'Max': 0
-    },
-    'ez-bar-wrist-curls': {
-        'Max': 0
-    },
-    'reverse-wrist-curls': {
-        'Max': 0
-    },
-    'squat-press': {
-        'Max': 0
-    },
-    'barbell-rdl': {
-        'Max': 0
-    },
-    'hip-thrust': {
-        'Max': 0
-    },
-    'adductor-machine': {
-        'Max': 0
-    },
-    'abduction-machine': {
-        'Max': 0
-    },
-    'leg-extensions': {
-        'Max': 0
-    },
-    'seated-leg-curls': {
-        'Max': 0
-    },
-    'preacher-curl-machine': {
-        'Max': 0
-    },
-    'tricep-pushdown': {
-        'Max': 0
-    },
-    'concentration-curls': {
-        'Max': 0
-    },
-    'jm-press-(smith)': {
-        'Max': 0
-    },
-    'db-hammer-curls': {
-        'Max': 0
-    },
-    'tricep-overhead-extensions-(cable)': {
-        'Max': 0
-    },
-    'straight-leg-calf-raise-machine': {
-        'Max': 0
-    },
-    'tibialis-anterior-raise': {
-        'Max': 0
-    },
-    'seated-calf-raise': {
-        'Max': 0
-    },
-    'incline-press-machine': {
-        'Max': 0
-    },
-    'decline-press-machine': {
-        'Max': 0
-    },
-    'db-shoulder-press': {
-        'Max': 0
-    },
-    'chest-flye-machine': {
-        'Max': 0
-    },
-    'cable-lateral-raises': {
-        'Max': 0
-    },
-    'pulldown-machine-(supinated-grip)': {
-        'Max': 0
-    },
-    'chest-supported-lat-row': {
-        'Max': 0
-    },
-    'chest-supported-t-bar-rows-(upper-back)': {
-        'Max': 0
-    },
-    'straight-arm-pulldowns-(cable)': {
-        'Max': 0
-    },
-    'bent-over-rear-delt-flyes': {
-        'Max': 0
-    },
-    'walked-out-cable-curls': {
-        'Max': 0
-    },
-    'pendulum-squat-(quad-version)': {
-        'Max': 0
-    },
-    'db-rdl': {
-        'Max': 0
-    },
-    'laying-leg-curl': {
-        'Max': 0
-    },
-    'hyperextension': {
-        'Max': 0
-    },
-    'incline-db-curls': {
-        'Max': 0
-    },
-    'unknown': {
-        'Max': 'unknown'
-    }
-}
+// const lifts = {
+//     'slight-incline-db-press': {
+//         'Max': 0
+//     },
+//     'seated-cable-chest-flyes': {
+//         'Max': 0
+//     },
+//     'dip-machine': {
+//         'Max': 0
+//     },
+//     'high-incline-db-press': {
+//         'Max': 0
+//     },
+//     'lateral-raises-(db)': {
+//         'Max': 0
+//     },
+//     'around-the-world-superman-hold': {
+//         'Max': 0
+//     },
+//     'single-arm-pulldown-(lats)': {
+//         'Max': 0
+//     },
+//     'single-arm-cable-row': {
+//         'Max': 0
+//     },
+//     'wide-grip-pulldowns': {
+//         'Max': 0
+//     },
+//     'chest-supported-upper-back-rows': {
+//         'Max': 0
+//     },
+//     'dumbbell-shrug': {
+//         'Max': 0
+//     },
+//     'rear-delt-flye-machine': {
+//         'Max': 0
+//     },
+//     'db-standing-curls': {
+//         'Max': 0
+//     },
+//     'db-hammer-curls': {
+//         'Max': 0
+//     },
+//     'reverse-grip-ez-bar-curls': {
+//         'Max': 0
+//     },
+//     'ez-bar-wrist-curls': {
+//         'Max': 0
+//     },
+//     'reverse-wrist-curls': {
+//         'Max': 0
+//     },
+//     'squat-press': {
+//         'Max': 0
+//     },
+//     'barbell-rdl': {
+//         'Max': 0
+//     },
+//     'hip-thrust': {
+//         'Max': 0
+//     },
+//     'adductor-machine': {
+//         'Max': 0
+//     },
+//     'abduction-machine': {
+//         'Max': 0
+//     },
+//     'leg-extensions': {
+//         'Max': 0
+//     },
+//     'seated-leg-curls': {
+//         'Max': 0
+//     },
+//     'preacher-curl-machine': {
+//         'Max': 0
+//     },
+//     'tricep-pushdown': {
+//         'Max': 0
+//     },
+//     'concentration-curls': {
+//         'Max': 0
+//     },
+//     'jm-press-(smith)': {
+//         'Max': 0
+//     },
+//     'db-hammer-curls': {
+//         'Max': 0
+//     },
+//     'tricep-overhead-extensions-(cable)': {
+//         'Max': 0
+//     },
+//     'straight-leg-calf-raise-machine': {
+//         'Max': 0
+//     },
+//     'tibialis-anterior-raise': {
+//         'Max': 0
+//     },
+//     'seated-calf-raise': {
+//         'Max': 0
+//     },
+//     'incline-press-machine': {
+//         'Max': 0
+//     },
+//     'decline-press-machine': {
+//         'Max': 0
+//     },
+//     'db-shoulder-press': {
+//         'Max': 0
+//     },
+//     'chest-flye-machine': {
+//         'Max': 0
+//     },
+//     'cable-lateral-raises': {
+//         'Max': 0
+//     },
+//     'pulldown-machine-(supinated-grip)': {
+//         'Max': 0
+//     },
+//     'chest-supported-lat-row': {
+//         'Max': 0
+//     },
+//     'chest-supported-t-bar-rows-(upper-back)': {
+//         'Max': 0
+//     },
+//     'straight-arm-pulldowns-(cable)': {
+//         'Max': 0
+//     },
+//     'bent-over-rear-delt-flyes': {
+//         'Max': 0
+//     },
+//     'walked-out-cable-curls': {
+//         'Max': 0
+//     },
+//     'pendulum-squat-(quad-version)': {
+//         'Max': 0
+//     },
+//     'db-rdl': {
+//         'Max': 0
+//     },
+//     'laying-leg-curl': {
+//         'Max': 0
+//     },
+//     'hyperextension': {
+//         'Max': 0
+//     },
+//     'incline-db-curls': {
+//         'Max': 0
+//     },
+//     'unknown': {
+//         'Max': 'unknown'
+//     }
+// }
 
-module.exports = {
-    connectToDb: (uri) => {
-        MongoClient.connect(db)
-        .then((client) => {
-            dbConnection = client.db()
-            return cb()
-        })
-        .catch(err => {
-            console.log(err)
-            return cb(err)
-        })
-    },
-    getDb: () => dbConnection
-}
+// module.exports = {
+//     connectToDb: (uri) => {
+//         MongoClient.connect(db)
+//         .then((client) => {
+//             dbConnection = client.db()
+//             return cb()
+//         })
+//         .catch(err => {
+//             console.log(err)
+//             return cb(err)
+//         })
+//     },
+//     getDb: () => dbConnection
+// }
+
+
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/index.html')
+    console.log(db.collection('lifts'))
 })
 
 app.get('/day1.html', (req, res)=>{
