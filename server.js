2542836478d6c2a6623dfbd1da63d060e5ba4df5
DylanFerger <fergdyl@gmail.com>
2026-03-29 13:06:26 -0400

    fixed db connection

--- server.js
+++ server.js
@@ -6,9 +6,66 @@ const path = require('path')
 const { MongoClient } = require('mongodb')
 
 let dbConnection
-let db = 'mongodb+srv://liftingApp:password123!@lifting-app-db.o3bapio.mongodb.net/?appName=lifting-app-db'
+const uri = 'mongodb+srv://liftingApp:password123!@lifting-app-db.o3bapio.mongodb.net/?appName=lifting-app-db'
 
 app.use(cors())
+
+MongoClient.connect(uri)
+  .then(client => {
+    dbConnection = client.db('lifts') // IMPORTANT
+    app.listen(PORT, () => {
+      console.log('Server running')
+    })
+  })
+  .catch(err => console.log(err))
+
+app.get('/api', (req, res) => {
+    dbConnection.collection('lifting-app-db').find().toArray()
+    .then(data => res.json(data))
+    .catch(err => {
+        console.log(err)
+        res.status(500).send('Error fetching data')
+    })
+})
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/day1.html', (req, res)=>{
+    res.sendFile(__dirname + '/day1.html')
+})
+
+app.get('/day2.html', (req, res)=>{
+    res.sendFile(__dirname + '/day2.html')
+})
+
+app.get('/day3.html', (req, res)=>{
+    res.sendFile(__dirname + '/day3.html')
+})
+
+app.get('/day4.html', (req, res)=>{
+    res.sendFile(__dirname + '/day4.html')
+})
+
+app.get('/day5.html', (req, res)=>{
+    res.sendFile(__dirname + '/day5.html')
+})
+
+app.get('/day6.html', (req, res)=>{
+    res.sendFile(__dirname + '/day6.html')
+})
+
+app.get('/day7.html', (req, res)=>{
+    res.sendFile(__dirname + '/day7.html')
+})
+
+app.get('/day8.html', (req, res)=>{
+    res.sendFile(__dirname + '/day8.html')
+})
+
 // const lifts = {
 //     'slight-incline-db-press': {
 //         'Max': 0
@@ -161,70 +218,3 @@ app.use(cors())
 //         'Max': 'unknown'
 //     }
 // }
-
-// module.exports = {
-//     connectToDb: (uri) => {
-//         MongoClient.connect(db)
-//         .then((client) => {
-//             dbConnection = client.db()
-//             return cb()
-//         })
-//         .catch(err => {
-//             console.log(err)
-//             return cb(err)
-//         })
-//     },
-//     getDb: () => dbConnection
-// }
-
-
-
-app.use(express.static(path.join(__dirname, 'public')))
-
-app.get('/', (req, res)=>{
-    res.sendFile(__dirname + '/index.html')
-    db.colletion('lifts').find()
-    .then(data => {
-        console.log(res)
-    })
-    .catch(error => console.log(error))
-})
-
-app.get('/day1.html', (req, res)=>{
-    res.sendFile(__dirname + '/day1.html')
-})
-
-app.get('/day2.html', (req, res)=>{
-    res.sendFile(__dirname + '/day2.html')
-})
-
-app.get('/day3.html', (req, res)=>{
-    res.sendFile(__dirname + '/day3.html')
-})
-
-app.get('/day4.html', (req, res)=>{
-    res.sendFile(__dirname + '/day4.html')
-})
-
-app.get('/day5.html', (req, res)=>{
-    res.sendFile(__dirname + '/day5.html')
-})
-
-app.get('/day6.html', (req, res)=>{
-    res.sendFile(__dirname + '/day6.html')
-})
-
-app.get('/day7.html', (req, res)=>{
-    res.sendFile(__dirname + '/day7.html')
-})
-
-app.get('/day8.html', (req, res)=>{
-    res.sendFile(__dirname + '/day8.html')
-})
-
-app.get('/api', (req, res)=> {
-    res.json(lifts)
-})
-
-app.listen(process.env.PORT || PORT, ()=>{
-})
